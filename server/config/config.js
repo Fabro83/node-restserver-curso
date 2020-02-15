4c0e997ba72aa90316fbac521ba0bbf694c13bf3
Fabricio <fabricio.rambaldi@gmail.com>
2020-02-15 19:26:40 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,31 @@
 //Declaración de variables globales
 
+
 // ===============================
 // Puerto
 // ===============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ===============================
+// Entorno
+// ===============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ===============================
+// Base de Datos
+// ===============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+} else {
+
+    urlDB = 'mongodb+srv://fabro:uf8p5ceKzjZzhzve@cluster0-ampvl.mongodb.net/cafe';
+
+}
+
+process.env.URLDB = urlDB;
