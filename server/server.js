8ee168f29658edf14a60df73561fc41d211570ba
Fabricio <fabricio.rambaldi@gmail.com>
2020-02-16 15:06:31 -0300

    SEED

--- server/server.js
+++ server/server.js
@@ -7,12 +7,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(require('./routes/usuario'));
+//Configuración global de rutas
+app.use(require('./routes/index'));
+
+
 
 mongoose.connect(process.env.URLDB, {
     useNewUrlParser: true,
